5421a4fa0a170afbec8d48a3ede3a143238554b5
jreleaserbot <jreleaser@kordamp.org>
2023-05-12 10:15:39 +0530

    cardbox cardbox-0.4.0

--- Formula/cardbox.rb
+++ Formula/cardbox.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2023-04-05T18:13:15.266847+05:30
+# Generated with JReleaser 1.2.0 at 2023-05-12T10:15:25.314133+05:30
 class Cardbox < Formula
   desc "Create JARs from Android projects for jQAssistant."
-  homepage "https://github.com/redgreenio/cardbox"
-  url "https://github.com/redgreenio/cardbox/releases/download/cardbox-0.3.0/cardbox-0.3.0.zip"
-  version "0.3.0"
-  sha256 "e56515702f639679a63b51e767100bb77c93798e18e77acd2d0f99db44bc15e6"
+  homepage "https://github.com/LegacyCodeHQ/cardbox"
+  url "https://github.com/LegacyCodeHQ/cardbox/releases/download/cardbox-0.4.0/cardbox-0.4.0.zip"
+  version "0.4.0"
+  sha256 "d54b85e2c0df0a07ea253e01e9216970be7873305f2d57d23d0ae814a7ec6a44"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
@@ -16,6 +16,6 @@ class Cardbox < Formula
 
   test do
     output = shell_output("#{bin}/cardbox --version")
-    assert_match "0.3.0", output
+    assert_match "0.4.0", output
   end
 end
