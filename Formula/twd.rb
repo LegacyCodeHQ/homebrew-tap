27bfd9692c366a75626694539ae04fc9d960b1d6
jreleaserbot <jreleaser@kordamp.org>
2023-05-18 14:52:27 +0530

    twd twd-0.44.0

--- Formula/twd.rb
+++ Formula/twd.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2023-05-18T14:46:02.946916+05:30
+# Generated with JReleaser 1.2.0 at 2023-05-18T14:50:06.307352+05:30
 class Twd < Formula
   desc "Understand and break down large classes without breaking a sweat."
   homepage "https://github.com/LegacyCodeHQ/tumbleweed"
   url "https://github.com/LegacyCodeHQ/tumbleweed/releases/download/twd-0.44.0/twd-0.44.0.zip"
   version "0.44.0"
-  sha256 "999a8b93263df5d13f53a3c68c0f52e73c9b35fb7bf7525c57a596a86e0ac6fe"
+  sha256 "fb49a42217f5d7ff55d24e50e1ec105a25d8de819021d90ccdaa9fcac6b0ba39"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
