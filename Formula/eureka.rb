8a1365f263a1c708e55b8a3fbbbe9aadd393a230
jreleaserbot <jreleaser@kordamp.org>
2023-07-19 19:54:10 +0530

    eureka eureka-0.87.0

--- Formula/eureka.rb
+++ Formula/eureka.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2023-07-17T22:33:11.215492+05:30
+# Generated with JReleaser 1.2.0 at 2023-07-19T19:53:41.511356+05:30
 class Eureka < Formula
   desc "Understand and break down large classes without breaking a sweat."
   homepage "https://github.com/LegacyCodeHQ/eureka"
-  url "https://github.com/LegacyCodeHQ/eureka/releases/download/eureka-0.86.0/eureka-0.86.0.zip"
-  version "0.86.0"
-  sha256 "fbc957f77f9af61592a9dcf488fba09656a95e26b2e98d2518a430fde2636f30"
+  url "https://github.com/LegacyCodeHQ/eureka/releases/download/eureka-0.87.0/eureka-0.87.0.zip"
+  version "0.87.0"
+  sha256 "9e91c8362f0f017fec6a24c35e966ea08ca3b8fe341a98502021943a196debb9"
   license "Apache-2.0"
 
   depends_on "openjdk@11"
@@ -16,6 +16,6 @@ class Eureka < Formula
 
   test do
     output = shell_output("#{bin}/eureka --version")
-    assert_match "0.86.0", output
+    assert_match "0.87.0", output
   end
 end
